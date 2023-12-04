7717b13ad07e6bf6c9ddbaf96b812edd71644a46
Alexander Wiebe <alexanderjwiebe@gmail.com>
2023-12-03 23:32:50 -0600

    regex whitespace took a while to figure out

--- day04/day04p1.ts
+++ day04/day04p1.ts
@@ -6,14 +6,27 @@ export function day04p1(filename: string) {
     .readFileSync(filename)
     .toString()
     .split("\n")
-    .map((line) => {
-      [...line.matchAll(/Card (\d+):\s+(\d.* )\| (\d.*)$/g)].map(
+    .map((line) =>
+      [...line.matchAll(/Card\s+(\d+):\s+(\d.*)\s+\|\s+(\d.*)$/g)].map(
         ([_, card, winners, pick]) => {
           console.log(card, winners, pick);
+          console.log(winners.split(/\s+/));
+          console.log(pick.split(/\s+/));
+          console.log(
+            winners.split(/\s+/).filter((winner) => {
+              return pick.split(/\s+/).includes(winner);
+            })
+          );
+          const winnerCount = winners.split(/\s+/).filter((winner) => {
+            return pick.split(/\s+/).includes(winner);
+          }).length;
+          return winnerCount > 0 ? Math.pow(2, winnerCount - 1) : 0;
         }
-      );
-    });
+      )
+    )
+    .flat()
+    .reduce((a, b) => a + b, 0);
 }
-
+console.log(Math.pow(2, 0));
 console.log(day04p1("./day04/example.txt"));
-// console.log(day03p1("./day04/raw-data.txt"));
+console.log(day04p1("./day04/raw-data.txt"));
