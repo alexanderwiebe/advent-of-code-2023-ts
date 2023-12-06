655c33120337fdd2a69695d03e58ba95e3b9c1f1
Alexander Wiebe <alexanderjwiebe@gmail.com>
2023-12-05 23:19:29 -0600

    init commit and part 1 done

---
+++ day06/day06p1.ts
@@ -0,0 +1,30 @@
+import * as fs from "node:fs";
+
+export function day06p2(filename: string) {
+  /* Part 1 */
+
+  const formattedLines = fs
+    .readFileSync(filename)
+    .toString()
+    .split("\n")
+    .map((line) => line.split(/\s+/));
+  let waysToWin: number[] = [];
+  for (let i = 1; i < formattedLines[0].length; i++) {
+    let time = +formattedLines[0][i];
+    let distance = +formattedLines[1][i];
+    console.log(time, distance);
+    let permutations: number[] = [];
+    for (let pushDuration = 1; pushDuration < time; pushDuration++) {
+      const moveDuration = time - pushDuration;
+      if (moveDuration * pushDuration > distance) {
+        permutations.push(pushDuration);
+      }
+    }
+    console.log(permutations);
+    waysToWin.push(permutations.length);
+  }
+  return waysToWin.reduce((acc, race) => acc * race);
+}
+
+console.log(day06p2("./day06/example.txt"));
+console.log(day06p2("./day06/raw-data.txt"));
