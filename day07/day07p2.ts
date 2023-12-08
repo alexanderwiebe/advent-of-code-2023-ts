0c1b3e16016277e467102eb61c50a6d49d000a9e
Alexander Wiebe <alexanderjwiebe@gmail.com>
2023-12-07 20:47:07 -0600

    getting ready for day 7

---
+++ day07/day07p2.ts
@@ -0,0 +1,30 @@
+import * as fs from "node:fs";
+
+export function day07p1(filename: string) {
+  /* Part 1 */
+
+  const formattedLines = fs
+    .readFileSync(filename)
+    .toString()
+    .split("\n")
+    .map((line) => line.split(/\s+/));
+  let waysToWin: number[] = [];
+  for (let i = 1; i < formattedLines[0].length; i++) {
+    let time = +formattedLines[0][i];
+    let distance = +formattedLines[1][i];
+    console.log(time, distance);
+    let permutations: number[] = [];
+    for (let pushDuration = 1; pushDuration < time; pushDuration++) {
+      const moveDuration = time - pushDuration;
+      if (moveDuration * pushDuration > distance) {
+        permutations.push(pushDuration);
+      }
+    }
+    console.log(permutations);
+    waysToWin.push(permutations.length);
+  }
+  return waysToWin.reduce((acc, race) => acc * race);
+}
+
+console.log(day07p1("./day07/example.txt"));
+console.log(day07p1("./day07/raw-data.txt"));
