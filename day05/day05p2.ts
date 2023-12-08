bf955f09df7108faeb6c1b2a74b5eefa7cb71562
Alexander Wiebe <alexanderjwiebe@gmail.com>
2023-12-07 20:43:19 -0600

    might have to flip the inputRange loop with the formatted lines loop

--- day05/day05p2.ts
+++ day05/day05p2.ts
@@ -35,10 +35,9 @@ export function day05p2(filename: string) {
           if (line === "") {
             const retVal = {
               ...acc,
-              [SECTIONS[sectionIndex]]: acc[SECTIONS[sectionIndex]],
-              // .sort(
-              //   (a, b) => a[1] - b[1]
-              // ),
+              [SECTIONS[sectionIndex]]: acc[SECTIONS[sectionIndex]].sort(
+                (a, b) => a[1] - b[1]
+              ),
             };
             sectionIndex++;
             return retVal;
@@ -76,42 +75,65 @@ export function day05p2(filename: string) {
   for (let i = 0; i < formattedLines.seeds.length; i += 2) {
     seedRange.push([seeds[i], seeds[i + 1]]);
   }
-  // console.log(seedRange);
+  console.log(seedRange);
 
-  let locationRange = seedRange.map(([start, inc]: any) => {
-    let locationRange: any = [];
-    // do stuff
-    for (let seedR = start; seedR < start + inc; seedR++) {
-      let seedMutate = seedR;
-      MAPPERS.forEach((section) => {
-        let listTransforms = formattedLines[section].filter(
-          ([_, source, inc]) => {
-            if (seedMutate >= source && seedMutate < source + inc) {
-              return true;
-            } else {
-              return false;
+  let locationRange = seedRange.map(([seedStart, seedInc]: any) => {
+    // map each seed range
+    let inputRanges: any = [[seedStart, seedStart + seedInc]];
+    let newInputRanges: any = [];
+    MAPPERS.forEach((section) => {
+      // for each section
+      inputRanges.forEach(([inputStart, inputEnd]) => {
+        formattedLines[section].forEach(
+          ([destStart, mapStart, mapInc], index, array) => {
+            // for each mapper
+
+            const mapEnd = mapStart + mapInc;
+            console.log(index); // , inputRanges[0][0]);
+            if (index === 0 && mapStart > 0 && inputRanges[0][0] < mapStart) {
+              newInputRanges.push([inputRanges[0][0], mapStart]); //figure out how to handle first case
+            }
+            if (index === array.length - 1 && inputRanges[0][0] >= mapEnd) {
+              newInputRanges.push([inputRanges[0][0], inputRanges[0][1]]);
+            }
+            // for each
+            if (inputStart <= mapStart && inputEnd >= mapEnd) {
+              // input range covers map range
+              newInputRanges.push([destStart, destStart + mapInc]);
+            }
+            if (inputStart >= mapStart && inputEnd <= mapEnd) {
+              // map range covers input range
+              newInputRanges.push([
+                inputStart - mapStart + destStart,
+                inputEnd - mapStart + destStart,
+              ]);
+            }
+            if (inputStart <= mapEnd && inputEnd >= mapEnd) {
+              // input range ends after map range
+              newInputRanges.push([inputStart - mapStart + destStart, mapEnd]);
+            }
+            if (inputStart <= mapStart && inputEnd >= mapStart) {
+              // input range starts before map range
+              newInputRanges.push([destStart, inputEnd - mapStart + destStart]);
             }
           }
-        );
-        if (listTransforms.length > 1) {
-          console.error("more than 1 transform found");
-        }
-        if (listTransforms.length === 0) {
-          // console.error("no transform found"); // now expected behaviour
-        } else {
-          let [dest, source] = listTransforms[0];
-          seedMutate = seedMutate - source + dest;
-        }
-      });
-      locationRange.push(seedMutate);
-    }
-    return locationRange;
-  });
-  // console.log(locationRange);
-  // console.log(Math.min(...locationRange.flat()));
+        ); // for each mapper
 
-  return Math.min(...locationRange.flat());
+        console.log("inputRanges", inputRanges);
+        console.log("newInputRanges", newInputRanges);
+        inputRanges = [...newInputRanges];
+        newInputRanges = [];
+        console.log("inputRanges", inputRanges);
+        console.log("newInputRanges", newInputRanges);
+      });
+    }); // for each section
+
+    return inputRanges;
+  }); // map each seed range
+  console.log(locationRange);
+
+  return Math.min(...locationRange.flat(2));
 }
 
-// console.log(day05p2("./day05/example.txt"));
+console.log(day05p2("./day05/example.txt"));
 console.log(day05p2("./day05/raw-data.txt"));
